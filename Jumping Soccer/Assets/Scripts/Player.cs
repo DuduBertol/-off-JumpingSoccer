cbf59f6a788066230a747957d9f13e0b7b2830b0
DuduBertolDev <dudubertoldev@gmail.com>
2023-12-20 13:08:46 -0300

    Prototype 01

--- Jumping Soccer/Assets/Scripts/Player.cs
+++ Jumping Soccer/Assets/Scripts/Player.cs
@@ -4,15 +4,27 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] bool isJumping;
+    [SerializeField] float jumpForce;
+    [SerializeField] Animator anim;
+    public Rigidbody2D rig;
+
+    public void Jump()
     {
+        anim.Play("kick");
+        if(!isJumping)
+        {
+            rig.velocity = Vector2.up * jumpForce;
+            isJumping = true;
+        }
         
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnCollisionEnter2D(Collision2D collision) 
     {
-        
+        if(collision.gameObject.layer == 6)
+        {
+            isJumping = false;
+        }
     }
 }
